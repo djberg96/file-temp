0b4b07575c5b73cf9182e3c995073560f589ccd5
unknown <djberge@.(none)>
2009-10-20 06:30:20 -0600

    Added Windows function definitions and switched to ANSI functions

--- lib/file/temp.rb
+++ lib/file/temp.rb
@@ -10,26 +10,43 @@ class File::Temp < File
   WINDOWS = Config::CONFIG['host_os'] =~ /mswin|win32|dos|cygwin|mingw/i
 
   if WINDOWS
-    ffi_lib 'msvcrt'  
+    ffi_lib 'msvcrt'
 
+    attach_function '_close', [:int], :int
+    attach_function 'fclose', [:pointer], :int
+    attach_function '_fdopen', [:int, :string], :pointer
     attach_function '_fileno', [:pointer], :int
-    attach_function '_umask', [:int], :int
     attach_function '_open', [:string, :int, :int], :int
+    attach_function '_open_osfhandle', [:long, :int], :int
+    attach_function 'tmpnam', [:string], :string
+    attach_function '_umask', [:int], :int
 
-    S_IWRITE = 128 # write permission, owner
-    S_IREAD  = 256 # read permission, owner
-    
-    BINARY      = 0x8000 # binary mode
-    SHORT_LIVED = 0x1000 # temporary storage, try not to flush
+    ffi_lib 'kernel32'
+
+    attach_function 'CloseHandle', [:long], :bool
+    attach_function 'CreateFileA', [:string, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :long
+    attach_function 'DeleteFileA', [:string], :bool
+    attach_function 'GetTempPathA', [:long, :string], :long
+    attach_function 'GetTempFileNameA', [:string, :string, :uint, :pointer], :uint
+
+    S_IWRITE = 128
+    S_IREAD = 256
+    BINARY = 0x8000
+    SHORT_LIVED = 0x1000
+    GENERIC_READ = 0x80000000
+    GENERIC_WRITE = 0x40000000
+    CREATE_ALWAYS = 2
+    FILE_ATTRIBUTE_NORMAL = 0x00000080
+    FILE_FLAG_DELETE_ON_CLOSE = 0x04000000
+    INVALID_HANDLE_VALUE = -1
   else
     attach_function 'fileno', [:pointer], :int
     attach_function 'mkstemp', [:string], :int
     attach_function 'umask', [:int], :int
     attach_function 'tmpfile', [], :pointer
+    attach_function 'fclose', [:pointer], :int
+    attach_function 'tmpnam', [:string], :string
   end
-
-  attach_function 'fclose', [:pointer], :int
-  attach_function 'tmpnam', [:string], :string
 
   public
 
@@ -38,7 +55,7 @@ class File::Temp < File
 
   if WINDOWS
     # The temporary directory used on your system.
-    TMPDIR = ENV['TEMP'] || ENV['TMP'] || ENV['TMPDIR'] || "C:\\Windows\\Temp"
+    TMPDIR = ENV['TEMP'] || ENV['TMP'] || ENV['USERPROFILE'] || "C:\\Windows\\Temp"
   else
     # The temporary directory used on your system.
     TMPDIR = ENV['TEMP'] || ENV['TMP'] || ENV['TMPDIR'] || '/tmp'
@@ -113,9 +130,6 @@ class File::Temp < File
 
   private
 
-  # The MS C runtime does not define a mkstemp() function, so we've
-  # created one here.
-  #
   if WINDOWS
     # The version of tmpfile() that ships with MS Windows has security
     # issues on Windows 7 and later, along with undesirable behavior in
@@ -125,34 +139,34 @@ class File::Temp < File
     # TODO: Add needed function definitions and tests.
     #
     def tmpfile
-      buf = 0.chr * 1024 
+      buf = 1.chr * 1024 
 
-      if GetTempPathW(buf.length, buf) == 0
+      if GetTempPathA(buf.length, buf) == 0
         raise SystemCallError, 'GetTempPath()'
       end
 
-      file_name = buf.strip
-      buf = 0.chr * 1024
+      file_name = buf[ /^[^\0]*/ ].chop # remove trailing slash
+      buf = 1.chr * 1024
 
-      if GetTempFileNameW(file_name, 'rb_', 0, buf) == 0
+      if GetTempFileNameA(file_name, 'rb_', 0, buf) == 0
         raise SystemCallError, 'GetTempFileName()'
       end
 
-      file_name = buf.strip
+      file_name = buf[ /^[^\0]*/ ]
 
-      handle = CreateFileW(
+      handle = CreateFileA(
         file_name,
         GENERIC_READ | GENERIC_WRITE,
         0,
         nil,
         CREATE_ALWAYS,
         FILE_ATTRIBUTE_NORMAL | FILE_FLAG_DELETE_ON_CLOSE,
-        nil 
+        0
       )
 
       if handle == INVALID_HANDLE_VALUE
-        DeleteFileW(file_name)
-        raise SystemCallError, 'CreateFileW()'
+        DeleteFileA(file_name)
+        raise SystemCallError, 'CreateFile()'
       end
 
       fd = _open_osfhandle(handle, 0)
@@ -172,6 +186,9 @@ class File::Temp < File
       fp
     end
 
+    # The MS C runtime does not define a mkstemp() function, so we've
+    # created one here.
+    #
     def mkstemp(template)
       flags = RDWR | BINARY | CREAT | EXCL | SHORT_LIVED
       pmode = S_IREAD | S_IWRITE
