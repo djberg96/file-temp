254ba16ddd30c37073a0c7dc9c8dc808f4a51421
Daniel Berger <djberg96@gmail.com>
2012-06-26 09:01:40 -0500

    Added documentation back to the initialize method.

--- lib/file/temp.rb
+++ lib/file/temp.rb
@@ -8,6 +8,29 @@ class File::Temp < File
 
   attr_reader :path
 
+  # Creates a new, anonymous, temporary file in your File::Temp::TMPDIR
+  # directory.
+  #
+  # If the +delete+ option is set to true (the default) then the temporary file
+  # will be deleted automatically as soon as all references to it are closed.
+  # Otherwise, the file will live on in your File::Temp::TMPDIR path.
+  #
+  # If the +delete+ option is set to false, then the file is not deleted. In
+  # addition, you can supply a string +template+ that the system replaces with
+  # a unique filename. This template should end with 3 to 6 'X' characters.
+  # The default template is 'rb_file_temp_XXXXXX'. In this case the temporary
+  # file lives in the directory where it was created.
+  #
+  # Note that when using JRuby the template naming is not as strict, and the
+  # trailing 'X' characters are simply replaced with the GUID that Java
+  # generates for unique file names.
+  #
+  # Example:
+  #
+  #    fh = File::Temp.new(true, 'rb_file_temp_XXXXXX') => file
+  #    fh.puts 'hello world'
+  #    fh.close
+  #
   def initialize(delete = true, template = 'rb_file_temp_XXXXXX')
     raise TypeError unless template.is_a?(String)
 
